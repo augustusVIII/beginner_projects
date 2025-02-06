1705e56c9bc733f5dbeac069d87efade4278ff58
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2025-02-06 17:17:12 +0700

    Add files via upload
    Add a method to convert all character into lowercase: .lower()

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@
 name = input("tell ur name: ")
 print("Hello " + name + " welcome to my game!")
 
-should_we_play = input("Do you want to play?: ")
+should_we_play = input("Do you want to play?: ").lower()
 play = should_we_play == "yes"
 
 if (play):
