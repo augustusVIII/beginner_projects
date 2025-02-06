fffc471e6074e3d47b7a1aa9b1a32e73bb7a435a
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2025-02-06 20:02:36 +0700

    Add files via upload
    add more options to the game & debug

--- project1.py
+++ project1.py
@@ -1,11 +1,26 @@
+import path
 
 name = input("tell ur name: ")
 print("Hello " + name + " welcome to my game!")
 
 should_we_play = input("Do you want to play?: ").lower()
-play = should_we_play == "yes"
+play = should_we_play == ("yes" or "y")
 
 if play:
     print("We will play")
+    weapon = input("choose a weapon (bow/sword)").lower()
+    pathway = input("please choose a path (swim/ bridge)").lower()
+    if pathway == "swim":
+        if weapon == "sword":
+            print("You met a crocodile and killed it with your sword, successfully crossing the river")
+        else:
+            print("You got killed by a crocodile, game end!")
+
+    elif pathway == "bridge":
+        print("You walked cross the river successfully")
+
+    else:
+        print("wrong input, you die, game end!")
+
 else:
     print("We won't play")
