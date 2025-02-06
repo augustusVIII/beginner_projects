3ed983a95c3c4f15d6d7b252495002521e5a0d98
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2025-02-06 17:08:27 +0700

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,10 @@
+
+name = input("tell ur name: ")
+print("Hello " + name + " welcome to my game!")
+
+should_we_play = input("Do you want to play?: ")
+play = should_we_play == "yes"
+
+if (play):
+    print
+
