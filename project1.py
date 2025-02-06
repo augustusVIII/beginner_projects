d0c5b0a7694eccdbb0e655e9cd3644bccba7d50a
augustusVIII <175667010+augustusVIII@users.noreply.github.com>
2025-02-06 17:18:22 +0700

    Add files via upload
    add method .lower()

--- project1.py
+++ project1.py
@@ -5,7 +5,7 @@ print("Hello " + name + " welcome to my game!")
 should_we_play = input("Do you want to play?: ").lower()
 play = should_we_play == "yes"
 
-if (play):
+if play:
     print("We will play")
 else:
     print("We won't play")
